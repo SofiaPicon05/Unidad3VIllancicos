4f7d878cd6d8977654d8d73e8c3226e5251f4e90
Frida Sofia Uñate Piña <201G0287@rcarbonifera.tecnm.mx>
2023-10-25 15:48:28 -0600

    Octubre/25/2023 :)

--- VIllancicos/Areas/Admin/Controllers/HomeController.cs
+++ VIllancicos/Areas/Admin/Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VIllancicos.Repositories;
 
 namespace VIllancicos.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
-        [Area("Admin")]
-        public IActionResult Index()
+		private readonly VillancicoRepository repository;
+
+		public HomeController(VillancicoRepository repository)
+        {
+			this.repository = repository;  // crear una dependencia , le diras al constructor q te de un repositorio para poder crearlo, se inyecta.
+		}
+		[Area("Admin")]
+		public IActionResult Index()
         {
             return View();
         }
---
+++ VIllancicos/Areas/Admin/Models/AdminIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VIllancicos.Areas.Admin.Models
+{
+	public class AdminIndexViewModel
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; } = null!;
+
+	}
+}
