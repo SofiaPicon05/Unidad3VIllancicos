6fb712566de1e5262ffdbb46fe6597aff2f0bb5b
Frida Sofia Uñate Piña <201G0287@rcarbonifera.tecnm.mx>
2023-10-26 15:55:31 -0600

    Octubre/26/2023

--- VIllancicos/Areas/Admin/Controllers/HomeController.cs
+++ VIllancicos/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VIllancicos.Areas.Admin.Models;
 using VIllancicos.Repositories;
 
 namespace VIllancicos.Areas.Admin.Controllers
@@ -6,7 +7,6 @@ namespace VIllancicos.Areas.Admin.Controllers
     public class HomeController : Controller
     {
 		private readonly VillancicoRepository repository;
-
 		public HomeController(VillancicoRepository repository)
         {
 			this.repository = repository;  // crear una dependencia , le diras al constructor q te de un repositorio para poder crearlo, se inyecta.
@@ -14,7 +14,13 @@ namespace VIllancicos.Areas.Admin.Controllers
 		[Area("Admin")]
 		public IActionResult Index()
         {
-            return View();
+            var data = repository.Get().Select(x => new AdminIndexViewModel
+            {
+                Id = x.Id,
+                Nombre = x.Nombre
+            }); 
+
+            return View(data);
         }
     }
 }
---
+++ VIllancicos/Areas/Admin/Controllers/VillancicosController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Core.Types;
+using Villancicos.Models;
+using VIllancicos.Models;
+using VIllancicos.Repositories;
+
+namespace VIllancicos.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class VillancicosController : Controller
+	{
+		private readonly VillancicoRepository Repository;
+		public VillancicosController(VillancicoRepository repos)  //inyeccion 
+		{
+			Repository = repos;
+		}
+
+
+
+		[HttpGet]
+		public IActionResult Agregar()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Agregar(Villancico v)
+		{
+			if (string.IsNullOrWhiteSpace(v.Nombre))
+			{
+				ModelState.AddModelError("", "Escriba el nombre del villancico");
+			}
+			if (string.IsNullOrWhiteSpace(v.Nombre))
+			{
+				ModelState.AddModelError("", "Escriba el nombre del compositor de villancico");
+			}
+			if (string.IsNullOrWhiteSpace(v.Nombre))
+			{
+				ModelState.AddModelError("", "Escriba la letra del villancico");
+			}
+			if (string.IsNullOrWhiteSpace(v.Nombre))
+			{
+				ModelState.AddModelError("", "Escriba la direccion del villancico en Youtube");
+			}
+			if (ModelState.IsValid)
+			{
+				Repository.Insert(v);
+				return RedirectToAction("Index", "Home", new { area = "Admin" });
+			}
+			else
+			return View(v);
+		}
+	}
+}
